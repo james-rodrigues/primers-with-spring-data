ecac85558b6ed1a65042bbebd40c8102bf608ab3
James Rodrigues <james.992rodrigues@gmail.com>
2021-05-16 20:49:33 +0530

    Initial Setup for Redis Data Primer

---
+++ primer-with-redis/src/main/java/com/sprimers/data/PrimerWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.sprimers.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerWithRedisApplication.class, args);
+	}
+
+}
---
+++ primer-with-redis/src/test/java/com/sprimers/data/PrimerWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprimers.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerWithRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
